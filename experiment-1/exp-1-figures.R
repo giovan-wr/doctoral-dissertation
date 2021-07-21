29077db9b5c64bbef65b92863533f903e039fe66
giovan-wr <giovanwillian@gmail.com>
2021-07-20 21:58:06 -0300

    figures adjustments

--- experiment-1/exp-1-figures.R
+++ experiment-1/exp-1-figures.R
@@ -1,9 +1,13 @@
-setwd("C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Resultados/Experimento 1/R")
 rm(list=ls())
+
+setwd("C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1")
+load("complete_data.RData")
 
 library(tidyverse)
 library(officer)
 library(rvg)
+
+font_size <- 11
 
 #### Wrangling#####
 create_list <- function(x){
@@ -58,25 +62,28 @@ summ_data_fst_blk$Block <- factor(summ_data_fst_blk$Block,
                                  levels=c('treinoab1','treinoac1','treinoad1',
                                           'mix1','eq1','adrev1','mixrev1','reorg1'))
 
-p <- summ_data_fst_blk %>% ggplot(aes(x=Block,y=Result,fill=Group)) +
-  geom_col(color='black', position = 'dodge', width = 0.8)
-
-p <- p + geom_line(aes(x=Block,y=Speed/0.008, group=Group),size=1) +
+summ_data_fst_blk %>% ggplot(aes(x=Block,y=Result,fill=Group)) +
+  geom_col(color='black', position = 'dodge', width = 0.8)+
+  geom_line(aes(x=Block,y=Speed/0.008, group=Group),size=1) +
   geom_point(aes(x=Block,y=Speed/0.008,shape=Group),size=3,fill='white',stroke =1) +
-  xlab('') + ylab('Acurácia (%)') +
-  scale_y_continuous(expand = c(0, 0),limits=c(0, 100), sec.axis=sec_axis(~.*0.008, name = 'Velocidade (r/s)'))
-
-p <- p + scale_x_discrete(labels = c('Treino AB','Treino AC','Treino AD','Revisão','Equivalência','Rev. AD / Tr. YZ','Revisão','Reorg. / Manut.')) +
-  scale_fill_manual(values=c("grey90","grey60"),name='',labels=c('Acurácia Reversão','Acurácia Controle')) +
-  scale_shape_manual(values = c(21,24),name='',labels = c('Vel. Reversão', 'Vel. Controle')) +
+  xlab('') + ylab('Acur?cia (%)') +
+  scale_y_continuous(expand = c(0, 0),limits=c(0, 100),
+                     sec.axis=sec_axis(~.*0.008, name = 'Velocidade (r/s)'))+
+  scale_x_discrete(labels = c('Treino AB','Treino AC','Treino AD','RevisÃ£o',
+                              'EquivalÃªncia','Rev. AD / Tr. YZ','RevisÃ£o',
+                              'Reorg. / Manut.')) +
+  scale_fill_manual(values=c("grey90","grey60"),
+                    name='',labels=c('AcurÃ¡cia ReversÃ£o','AcurÃ¡cia Controle')) +
+  scale_shape_manual(values = c(21,24),name='',
+                     labels = c('Vel. ReversÃ£o', 'Vel. Controle')) +
   theme_classic() + theme(axis.text=element_text(size=font_size),
                           axis.text.x = element_text(color='black',angle = 45,hjust = 1),
                           axis.ticks.x = element_blank(),axis.title.y=element_text(size=font_size),
                           axis.text.y = element_text(color='black'),legend.position = "bottom",
                           legend.text = element_text(size = font_size))
 
-ggsave('Eixo duplo.png',device = 'png',
-       path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+ggsave('Two axis.png',device = 'png',
+       path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
        width = 16, height = 12, units = "cm")
 
 
@@ -84,66 +91,52 @@ ggsave('Eixo duplo.png',device = 'png',
 
 #### Duplo eixo por participante ####
 
-individual_second_axis <-function(participant,group){
+summ_data_part <- complete_data %>% group_by(Group, Participant, Block) %>%
+  summarise(Result=mean(Result)*100,Speed=mean(Speed))
+
+individual_second_axis <-function(participant){
   
-  ifelse(group=='ADr',nome_blocos <- c('Treino AB','Treino AC','Treino AD','Revisão','Equivalência','Rev. AD','Revisão','Reorganização'),
-                        nome_blocos <- c('Treino AB','Treino AC','Treino AD','Revisão','Equivalência','Treino YZ','Revisão','Manutenção'))
+  p_data <- summ_data_part %>%
+    filter(Participant==participant,
+           Block %in% c('treinoab1','treinoac1',
+                        'treinoad1','mix1','eq1',
+                        'adrev1','yz1','mixrev1',
+                        'mixcontr1','reorg1','maint1'))
   
-  summ_data_part %>%
-    filter(Participant==participant,Group==group, Block %in% c('treinoab1','treinoac1',
-                                                               'treinoad1','mix1','eq1',
-                                                               'adrev1','yz1','mixrev1',
-                                                               'mixcontr1','reorg1','maint1')) %>%
+  group <- unique(p_data$Group)
+  
+  ifelse(group=='ADr',
+         nome_blocos <- c('Treino AB','Treino AC','Treino AD','RevisÃ£o','EquivalÃªncia','Rev. AD','RevisÃ£o','ReorganizaÃ§Ã£o'),
+         nome_blocos <- c('Treino AB','Treino AC','Treino AD','RevisÃ£o','EquivalÃªncia','Treino YZ','RevisÃ£o','ManutenÃ§Ã£o'))
+  
+  p_data %>%
     ggplot(aes(x=Block,y=Result)) +
     geom_col(color='black', fill='grey90', position = 'dodge') +
-    geom_line(aes(x=Block,y=Speed/ajuste_eixo,group = 'Block')) +
-    geom_point(aes(x=Block,y=Speed/ajuste_eixo),size=3,fill='white',shape =21) +
-    xlab('') + ylab('Acurácia (%)') +
+    geom_line(aes(x=Block,y=Speed/axis_adjust,group = 'Block')) +
+    geom_point(aes(x=Block,y=Speed/axis_adjust),size=3,fill='white',shape =21) +
+    xlab('') + ylab('AcurÃ¡cia (%)') +
     scale_y_continuous(expand = c(0, 0),limits=c(0, 100),
-                       sec.axis=sec_axis(~.*ajuste_eixo, name = 'Velocidade (r/s)'))+
+                       sec.axis=sec_axis(~.*axis_adjust, name = 'Velocidade (r/s)'))+
     scale_x_discrete(labels = nome_blocos) +
     theme_classic() + theme(axis.text=element_text(size=font_size),
                             axis.text.x = element_text(color='black',angle = 45,hjust = 1),
                             axis.ticks.x = element_blank(),axis.title.y=element_text(size=font_size),
                             axis.text.y = element_text(color='black'))+
     if (group=="ADr"){
-      ggtitle(paste('R.',participant,sep = '')) 
+      ggtitle(paste('R.',str_extract(participant, '[[:digit:]]+'),sep = '')) 
     } else {
-      ggtitle(paste('C.',participant,sep = '')) 
+      ggtitle(paste('C.',str_extract(participant, '[[:digit:]]+'),sep = '')) 
     }
   
-  ggsave(paste('eixoduplo_',group,'P',participant,'.png',sep = ''),device = 'png',
-         path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+  ggsave(paste('twoaxis_',group,str_extract(participant, '[[:digit:]]+'),'.png',sep = ''),
+         device = 'png',
+         path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
          width = 15, height = 10, units = "cm")
 }
 
-ajuste_eixo <- 0.008
-dir(pattern="AD")
-lapply(1:5,individual_second_axis,group='ADr')
+axis_adjust <- 0.008
 
-
-
-#### Figura da interação ####
-complete_data %>% filter(Block%in%c('eq1','reorg1','maint1')) %>%
-  mutate(Block=case_when(.$Block=='maint1'~'reorg1',TRUE~as.character(.$Block))) %>%
-  group_by(Group,Block) %>% summarise(Speed=mean(Speed)) %>%
-  
-  ggplot(aes(x=Block,y=Speed,group=Group,shape=Group, fill=Group)) +
-  geom_line() + geom_point(size=4) +
-  scale_shape_manual(values = c(21,21),labels=c('Grupo ADr','Grupo Controle')) +
-  scale_fill_manual(values = c('black','white'),labels=c('Grupo ADr','Grupo Controle')) +
-  xlab('Bloco') + ylab('Velocidade média (r/s)') +
-  scale_x_discrete(label=c('Equivalência','Reorg./Manut.'))+
-  scale_y_continuous(limits = c(0.36,0.52),expand = c(0,0)) +
-  theme_classic() +
-  theme(axis.title = element_text(size=12, color = 'black'),
-        axis.text = element_text(size = 12,color = 'black'),
-        legend.title = element_blank(),
-        legend.text = element_text(size = 12,color = 'black'))
-
-ggsave('Interação.png',device = 'png',
-       path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
-       width = 15, height = 8, units = "cm")
+lapply(unique(summ_data_part$Participant),individual_second_axis)
 
 
 #### Boxplot ####
@@ -154,17 +147,17 @@ complete_data %>% mutate(Block=case_when(
   filter(Block %in% c('eq1','reorg1')) %>%
   ggplot(aes(Block,Speed,fill=Group))+geom_boxplot()+
   scale_fill_manual(values=c("grey90","grey60"),name='')+
-  scale_x_discrete(labels = c('Equivalência','Reorg. / Manut.'))+
+  scale_x_discrete(labels = c('EquivalÃªncia','Reorg. / Manut.'))+
   xlab('') + ylab('Velocidade (r/s)') + theme_classic() +
   theme(axis.text=element_text(size=font_size,colour = 'black'),
         axis.title=element_text(size=font_size),
         legend.text = element_text(size = font_size))
 
 ggsave('Boxplot.png',device = 'png',
-       path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+       path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
        width = 15, height = 12, units = "cm")
 
-#### Densidade ####
+#### Density ####
 
 complete_data %>% mutate(Block=case_when(
   .$Block=='maint1'~'reorg1',
@@ -172,7 +165,7 @@ complete_data %>% mutate(Block=case_when(
 )) %>%
 filter(Block %in% c('eq1','reorg1')) %>%
   mutate(Block = case_when(
-    .$Block == 'eq1' ~ "Equivalência",
+    .$Block == 'eq1' ~ "EquivalÃªncia",
     .$Block == 'reorg1' ~ "Reorg./Manut.",
     TRUE~'Erro')) %>%
   ggplot(aes(x=Speed,fill=Group))+geom_density(outline.type = 'full',alpha=0.3)+
@@ -186,17 +179,10 @@ filter(Block %in% c('eq1','reorg1')) %>%
         strip.text.x = element_text(size = font_size))
 
 ggsave('Densidade.png',device = 'png',
-       path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+       path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
        width = 15, height = 10, units = "cm")
 
-#### Revisão com reversão AD  ####
-complete_data <- complete_data %>% mutate(hit_stm = case_when(
-  .$HitKey==1 ~ .$ChKey1,
-  .$HitKey==2 ~ .$ChKey2,
-  .$HitKey==3 ~ .$ChKey3,
-  .$HitKey==4 ~ .$ChKey4,
-  TRUE ~ "Error"
-))
+#### Review AD reversal  ####
 
 mixrev <- complete_data %>% filter(Block=='mixrev1') %>%
   mutate(trial_type = str_sub(hit_stm,1,1)) %>%
@@ -204,19 +190,19 @@ mixrev <- complete_data %>% filter(Block=='mixrev1') %>%
 
 AB <- mixrev %>% filter(trial_type=='AB') %>%
   group_by(BlkTrl) %>% summarise(Result=sum(Result)) %>%
-  mutate(Tentativas=1:9, Relação=rep('AB',9))
+  mutate(Tentativas=1:9, Relation=rep('AB',9))
 AC <- mixrev %>% filter(trial_type=='AC') %>%
   group_by(BlkTrl) %>% summarise(Result=sum(Result)) %>%
-  mutate(Tentativas=1:9, Relação=rep('AC',9))
+  mutate(Tentativas=1:9, Relation=rep('AC',9))
 AD <- mixrev %>% filter(trial_type=='AD') %>%
   group_by(BlkTrl) %>% summarise(Result=sum(Result)) %>%
-  mutate(Tentativas=1:9, Relação=rep('ADr',9))
+  mutate(Tentativas=1:9, Relation=rep('ADr',9))
 
 bind_rows(AB,AC,AD) %>%
-  ggplot(aes(Tentativas,Result,group=Relação,shape=Relação,
-             linetype=Relação, fill=Relação)) +
+  ggplot(aes(Tentativas,Result,group=Relation,shape=Relation,
+             linetype=Relation, fill=Relation)) +
   geom_line()+ scale_linetype_manual(values=c('solid','longdash','dotted'))+
-  geom_point(aes(size=Relação),stroke=1,fill='white') +
+  geom_point(aes(size=Relation),stroke=1,fill='white') +
   scale_shape_manual(values = c(21,24,22)) +
   scale_size_manual(values = c(4,3,3)) +
   coord_cartesian(c(1,9),c(0,5),clip = 'off')+
@@ -229,18 +215,18 @@ bind_rows(AB,AC,AD) %>%
         axis.text = element_text(color = 'black', size=font_size))
 
 ggsave('adrev.png',device = 'png',
-       path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+       path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
        width = 13, height = 8, units = "cm")
 
-#### Gráficos individuais (Velocidade por tentativa)#############
+#### Individual plots (Speed by trial)#############
 
-individual_plots <- function(participant,group){
+individual_plots <- function(participant){
   
   blocks_position <- complete_data %>% filter(Participant==participant,
-                                              Group==group,BlkTrl==1,
+                                              BlkTrl==1,
                                               grepl('1',Block)) %>% .$Trl
   blocks_names <- complete_data %>% filter(Participant==participant,
-                                           Group==group,BlkTrl==1,
+                                           BlkTrl==1,
                                            grepl('1',Block)) %>% .$Block
   positions<-data.frame(x=blocks_position,y=rep(3,length(blocks_position)),label=blocks_names)
   
@@ -262,30 +248,31 @@ individual_plots <- function(participant,group){
       TRUE ~ "Error")
     )
   
-  p <- complete_data %>% filter(Participant==participant,Group==group) %>%
+  group <- complete_data %>% filter(Participant==participant) %>% .$Group%>%unique()
+  
+  complete_data %>% filter(Participant==participant) %>%
     ggplot(aes(x=Trl,y=Speed))+
     geom_line(color='blue')+
     theme_classic() +
     geom_vline(xintercept = blocks_position)+
     geom_point(aes(color=as.factor(Result)))+scale_color_manual(values=c("red",NA))+
-    theme(legend.position = "none")+scale_x_continuous(expand = c(0, 0))
-  
-  p+geom_text(data=positions,aes(x=x,y=y,label=label),nudge_x = 15, nudge_y = -1) +
+    theme(legend.position = "none")+scale_x_continuous(expand = c(0, 0))+
+    geom_text(data=positions,aes(x=x,y=y,label=label),nudge_x = 15, nudge_y = -1) +
     xlab('Tentativa') + ylab('Velocidade') +
     theme(axis.text = element_text(colour = 'black',size=font_size))+
     if (group=="ADr"){
-      ggtitle(paste('R.',participant,sep = '')) 
+      ggtitle(paste('R.',str_extract(participant, '[[:digit:]]+'),sep = '')) 
     } else {
-      ggtitle(paste('C.',participant,sep = '')) 
+      ggtitle(paste('C.',str_extract(participant, '[[:digit:]]+'),sep = '')) 
     } 
   
-  ggsave(paste('velocidade',group,'P',participant,'.png',sep = ''),device = 'png',
-         path = "C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1",
+  ggsave(paste('speed',participant,'.png',sep = ''),device = 'png',
+         path = "C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures",
          width = 26, height = 7, units = "cm")
 }
 
-dir(pattern="AD")
-lapply(1:4,individual_plots,group='Controle')
+
+lapply(unique(complete_data$Participant),individual_plots)
 
 #### Wrangling matrix de respostas#######################################
 library(xlsx)
@@ -322,10 +309,10 @@ complete_data <- complete_data %>% mutate(Group = case_when(
   TRUE ~ "Error"
 ))
 
-r_matrix<- function(x,y,wb){
-  sheet <- createSheet(wb, paste(y,'P',x,sep = ''))
+r_matrix<- function(x,wb){
+  sheet <- createSheet(wb, paste(x,sep = ''))
   mixrev_data <- complete_data %>%
-    filter(Group==y,Participant==x) %>%
+    filter(Participant==x) %>%
     mutate(Block=case_when(
       .$Block=='mixcontr1' ~ 'mixrev1',
       .$Block=='mixcontr2' ~ 'mixrev2',
@@ -362,10 +349,10 @@ pre_matrix <- function(z){
   sheetTitle <- createCell(rows, colIndex=1)
   setCellValue (sheetTitle[[1,1]], z)
   
-  saveWorkbook (wb, "Matrizes de resposta.xlsx")
+  saveWorkbook (wb, "Responses matrix.xlsx")
 }
 
-setwd("C:/Users/Giovan WR/Documents/UFSCar/Doutorado/Qualificação/Experimento 1")
+setwd("C:/Users/Giovan WR/projects/doctoral-dissertation/experiment-1/figures")
 wb = createWorkbook()
-lapply(1:5, r_matrix,y='Controle',wb=wb)
+lapply(unique(complete_data$Participant), r_matrix,wb=wb)
 
